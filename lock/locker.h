a66c017bc8eb0bdacad7966a0dcd82dd886520b6
supermicro <supermicro@scnu.com>
2023-10-20 21:02:55 +0800

    线程同步机制包装类进一步注释

--- lock/locker.h
+++ lock/locker.h
@@ -5,118 +5,115 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-/* 封装信号量的类 */
-class sem
+// 封装信号量的类
+class sem 
 {
 public:
-    /* 创建并初始化信号量值为0 */
+    // 封装了sem_init来进行初始化，二进制信号量
     sem()
     {
-        // sem_init函数用于初始化一个未命名的信号量（成功返回0，失败返回errno）
-        if (sem_init(&m_sem, 0, 0) != 0)
+        if (sem_init(&m_sem, 0, 0) != 0) // 返回0表示没出错
         {
             throw std::exception();
         }
     }
 
-    /* 创建并初始化信号量值为num */
+    // 封装了sem_init来进行初始化，重载
     sem(int num)
     {
-        // sem_init函数用于初始化一个未命名的信号量（成功返回0，失败返回errno）
-        if (sem_init(&m_sem, 0, num) != 0)
+        if (sem_init(&m_sem, 0, num) != 0) // 返回0表示没出错
         {
             throw std::exception();
         }
     }
 
-    /* 销毁信号量 */
+    // 封装了sem_destroy来销毁信号量
     ~sem()
     {
-        // sem_destroy函数用于销毁信号量
         sem_destroy(&m_sem);
     }
 
-    /* 等待信号量 */
+    // 等待信号量
     bool wait()
     {
-        // sem_wait函数将以原子操作方式将信号量减一,信号量为0时,sem_wait阻塞（成功返回0，失败返回errno）
-        return sem_wait(&m_sem) == 0;
+        return sem_wait(&m_sem) == 0; // 返回0表示没出错
     }
 
-    /* 增加信号量 */
+    // 封装sem_post增加信号量的值
     bool post()
     {
-        // sem_post函数以原子操作方式将信号量加一,信号量大于0时,唤醒调用sem_post的线程（成功返回0，失败返回errno）
-        return sem_post(&m_sem) == 0;
+        return sem_post(&m_sem) == 0; // 返回0表示没出错
     }
 
 private:
-    sem_t m_sem; /* 存放信号量的值 */
+    sem_t m_sem; // 存放信号量的值
 };
 
-/* 封装互斥锁的类 */
+// 封装互斥锁的类
 class locker
 {
 public:
-    /* 创建并初始化互斥锁 */
+    // 创建并初始化互斥锁
     locker()
     {
-        if (pthread_mutex_init(&m_mutex, NULL) != 0)
+        if (pthread_mutex_init(&m_mutex, NULL) != 0) // 返回0表示没出错
         {
             throw std::exception();
         }
     }
 
-    /* 销毁互斥锁 */
+    // 销毁互斥锁
     ~locker()
     {
         pthread_mutex_destroy(&m_mutex);
     }
 
-    /* 获取互斥锁 */
+    // 获取互斥锁
     bool lock()
     {
-        return pthread_mutex_lock(&m_mutex) == 0;
+        return pthread_mutex_lock(&m_mutex) == 0; // 返回0表示没出错
     }
 
-    /* 释放互斥锁 */
+    // 释放互斥锁
     bool unlock()
     {
-        return pthread_mutex_unlock(&m_mutex) == 0;
+        return pthread_mutex_unlock(&m_mutex) == 0; // 返回0表示没出错
     }
 
-    /* 获取指向互斥锁的指针？ */
+    // 获取指向互斥锁的指针？
     pthread_mutex_t *get()
     {
         return &m_mutex;
     }
 
 private:
-    pthread_mutex_t m_mutex; /* 存放互斥锁的值 */
+    pthread_mutex_t m_mutex; // 存放互斥锁的值
 };
 
-/* 封装条件变量的类
-注意，这个跟书上的有一定差别，将互斥锁放在类外了 */
+// 封装条件变量的类
+// 注意，这个跟书上的有一定差别，将互斥锁放在类外了
 class cond
 {
 public:
-    /* 创建并初始化条件变量 */
+    // 创建并初始化条件变量
     cond()
     {
         if (pthread_cond_init(&m_cond, NULL) != 0)
         {
+            // 注意，这个跟书上的有一定差别，将互斥锁放在类外了
             // pthread_mutex_destroy(&m_mutex);
             throw std::exception();
         }
     }
 
-    /* 销毁条件变量 */
+    // 销毁条件变量
     ~cond()
     {
         pthread_cond_destroy(&m_cond);
     }
 
-    /*  */
+    // 等待条件变量
+    // 注意，这个跟书上的有一定差别，将互斥锁放在类外了
     bool wait(pthread_mutex_t *m_mutex)
     {
         int ret = 0;
@@ -125,6 +122,9 @@ public:
         // pthread_mutex_unlock(&m_mutex);
         return ret == 0;
     }
+
+    // 等待条件变量，带超时时间
+    // 注意，这个跟书上的有一定差别，将互斥锁放在类外了
     bool timewait(pthread_mutex_t *m_mutex, struct timespec t)
     {
         int ret = 0;
@@ -133,10 +133,14 @@ public:
         // pthread_mutex_unlock(&m_mutex);
         return ret == 0;
     }
+
+    // 唤醒等待条件变量的其中一个线程
     bool signal()
     {
         return pthread_cond_signal(&m_cond) == 0;
     }
+
+    // 唤醒等待条件变量的线程，广播
     bool broadcast()
     {
         return pthread_cond_broadcast(&m_cond) == 0;
@@ -144,6 +148,7 @@ public:
 
 private:
     // static pthread_mutex_t m_mutex;
-    pthread_cond_t m_cond;
+    pthread_cond_t m_cond; // 存放条件变量的值
 };
+
 #endif
--- main.cpp
+++ main.cpp
@@ -23,22 +23,24 @@
 #define SYNLOG // 同步写日志
 // #define ASYNLOG // 异步写日志
 
-// #define listenfdET /* 设置ET模式（在addfd函数中修改这个属性）*/
-#define listenfdLT /* 设置LT模式（在addfd函数中修改这个属性） */
+// #define listenfdET // 设置ET模式
+#define listenfdLT // 设置LT模式
 
-/* ET模式下epoll_wait通知事件发生后需要立即处理完毕这个事件的所有内容，因为后续不会再通知此事件
-具体可以看后面的if (sockfd == listenfd)部分的内容，很好地诠释了LT和ET工作模式*/
+/*
+知识点：ET模式下epoll_wait通知事件发生后需要立即处理完毕这个事件的所有内容，因为后续不会再通知此事件
+具体可以看后面的if (sockfd == listenfd)部分的内容，很好地诠释了LT和ET工作模式
+*/
 
-/* 下面三个函数在http_conn.cpp中定义 */
-extern int addfd(int epollfd, int fd, bool one_shot); /* 把fd加入到内核事件监听表epollfd中 */
-extern int remove(int epollfd, int fd);               /* 从内核事件监听表epollfd中移除fd */
-extern int setnonblocking(int fd);                    /* 设置fd的属性为非阻塞 */
+// 下面三个函数在http_conn.cpp中定义
+extern int addfd(int epollfd, int fd, bool one_shot); // 把fd加入到内核事件监听表epollfd中
+extern int remove(int epollfd, int fd);               // 从内核事件监听表epollfd中移除fd
+extern int setnonblocking(int fd);                    // 设置fd的属性为非阻塞
 
-static int pipefd[2];            /* 主线程和子线程之间通信用的管道 */
-static sort_timer_lst timer_lst; /* 定时器升序链表实例，静态的，只能在当前文件内使用 */
-static int epollfd = 0;          /* 标识内核事件监听表的文件描述符 */
+static int pipefd[2];            // 主线程和子线程之间通信用的管道
+static sort_timer_lst timer_lst; // 定时器升序链表实例，静态的，只能在当前文件内使用
+static int epollfd = 0;          // 标识内核事件监听表的文件描述符
 
-/* 传入一个信号值sig，将它通过管道发送给主线程 */
+// 传入一个信号值sig，将它通过管道发送给主线程
 void sig_handler(int sig)
 {
     // （不理解）为保证函数的可重入性，保留原来的errno
@@ -48,7 +50,7 @@ void sig_handler(int sig)
     errno = save_errno;
 }
 
-/* 监听某个信号sig，并设置其信号处理函数handler，restart参数设置中断前的系统调用是否重启 */
+// 监听某个信号sig，并设置其信号处理函数handler，restart参数设置中断前的系统调用是否重启
 void addsig(int sig, void(handler)(int), bool restart = true)
 {
     struct sigaction sa;
@@ -60,138 +62,133 @@ void addsig(int sig, void(handler)(int), bool restart = true)
     assert(sigaction(sig, &sa, NULL) != -1);
 }
 
-/* 定时处理任务，重新定时以不断触发SIGALRM信号 */
+// 定时处理任务，重新定时以不断触发SIGALRM信号
 void timer_handler()
 {
-    timer_lst.tick();
-    alarm(TIMESLOT);
+    timer_lst.tick(); // 检查链表上是否有到期任务
+    alarm(TIMESLOT);  // 过TIMESLOT秒后再次触发SIGALRM信号
 }
 
-/* 定时器回调函数，删除非活动连接在epollfd上的注册事件，并关闭 */
+// 定时器回调函数，删除非活动连接在epollfd上的注册事件，并关闭
 void cb_func(client_data *user_data)
 {
-    epoll_ctl(epollfd, EPOLL_CTL_DEL, user_data->sockfd, 0); /* 取消监听 */
+    epoll_ctl(epollfd, EPOLL_CTL_DEL, user_data->sockfd, 0); // 取消监听
     assert(user_data);
-
-    close(user_data->sockfd); /* 关闭socket连接 */
-
-    http_conn::m_user_count--; /* http连接个数相应减少 */
-
-    LOG_INFO("close fd %d", user_data->sockfd); /* 输出日志 */
-    Log::get_instance()->flush();
+    close(user_data->sockfd);                   // 关闭socket连接
+    http_conn::m_user_count--;                  // http连接个数相应减少
+    LOG_INFO("close fd %d", user_data->sockfd); // 输出日志
+    Log::get_instance()->flush();               // 强制刷新缓冲区
 }
 
-/* 给connfd发送info错误信息，然后关闭connfd对应的socket连接 */
+// 给connfd发送info错误信息，然后关闭connfd对应的socket连接
 void show_error(int connfd, const char *info)
 {
-    printf("%s", info);
-    send(connfd, info, strlen(info), 0);
-    close(connfd);
+    printf("%s", info);                  // 输出错误信息
+    send(connfd, info, strlen(info), 0); // 发送错误信息
+    close(connfd);                       // 关闭socket连接
 }
 
 int main(int argc, char *argv[])
 {
-#ifdef ASYNLOG /* 异步日志 */
-    Log::get_instance()->init("ServerLog", 2000, 800000, 8);
+#ifdef ASYNLOG
+    Log::get_instance()->init("ServerLog", 2000, 800000, 8); // 初始化异步日志
 #endif
 
-#ifdef SYNLOG /* 同步日志 */
-    Log::get_instance()->init("ServerLog", 2000, 800000, 0);
+#ifdef SYNLOG
+    Log::get_instance()->init("ServerLog", 2000, 800000, 0); // 初始化同步日志
 #endif
 
-    /* 判断程序输入参数是否正确 */
-    if (argc <= 1)
+    if (argc <= 1) // 没有输入端口号
     {
-        printf("usage: %s ip_address port_number\n", basename(argv[0]));
+        printf("usage: %s ip_address port_number\n", basename(argv[0])); // basename()返回路径中的文件名部分
         return 1;
     }
 
-    int port = atoi(argv[1]); /* 从argv获得端口号 */
+    int port = atoi(argv[1]); // Convert a string to an integer
 
-    addsig(SIGPIPE, SIG_IGN); /* 监听到SIGPIPE信号直接忽略 */
+    addsig(SIGPIPE, SIG_IGN); // 监听到SIGPIPE信号直接忽略
 
-    connection_pool *connPool = connection_pool::GetInstance(); /* 指向数据库连接池唯一实例的指针变量 */
+    connection_pool *connPool = connection_pool::GetInstance();  // 指向数据库连接池唯一实例的指针变量
+    connPool->init("localhost", "root", "root", "web", 3306, 8); // 数据库连接池初始化
 
-    connPool->init("localhost", "root", "root", "web", 3306, 8); /* 数据库连接池初始化 */
-
-    threadpool<http_conn> *pool = NULL; /* 指向http_conn类型线程池的指针变量 */
+    threadpool<http_conn> *pool = NULL; // 指向http_conn类型线程池的指针变量
     try
     {
-        pool = new threadpool<http_conn>(connPool);
-    } /* 线程池初始化 */
-    catch (...)
+        pool = new threadpool<http_conn>(connPool); // 创建线程池
+    }
+    catch (...) // 捕获所有异常
     {
         return 1;
     }
 
-    http_conn *users = new http_conn[MAX_FD]; /* 存放http_conn连接用户的数据，可以用fd来索引对应用户数据 */
+    http_conn *users = new http_conn[MAX_FD]; // 用于存放所有可能的http_conn连接用户的数据，可以用fd来索引对应用户数据
     assert(users);
 
-    /* 作用仅仅是从数据库中取出用户名和密码
-    注意一下，存放用户名和密码的map容器变量名也是users，不要搞混了 */
+    // 作用仅仅是从数据库中取出用户名和密码
+    // 注意一下，存放用户名和密码的map容器变量名也是users，不要搞混了
     users->initmysql_result(connPool);
 
-    int listenfd = socket(PF_INET, SOCK_STREAM, 0); /* 主线程中的监听描述符 */
+    int listenfd = socket(PF_INET, SOCK_STREAM, 0); // 主线程中的监听描述符
     assert(listenfd >= 0);
 
     // struct linger tmp={1,0};
     // SO_LINGER若有数据待发送，延迟关闭
     // setsockopt(listenfd,SOL_SOCKET,SO_LINGER,&tmp,sizeof(tmp));
 
-    int ret = 0; /* 存放系统调用返回值的临时变量 */
+    int ret = 0; // 存放系统调用返回值的临时变量
 
-    struct sockaddr_in address; /* 存放socket地址的变量，包含协议族，IP地址，端口号 */
+    struct sockaddr_in address; // 存放socket地址的变量，包含协议族，IP地址，端口号
     bzero(&address, sizeof(address));
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = htonl(INADDR_ANY); /* INADDR_ANY：Address to accept any incoming messages. */
+    address.sin_addr.s_addr = htonl(INADDR_ANY); // INADDR_ANY：Address to accept any incoming messages.
     address.sin_port = htons(port);
 
-    /* 连接关闭后不用TIME_WAIT可以立即重用刚关闭的socket使用的IP和端口号 */
+    // 连接关闭后不用TIME_WAIT可以立即重用刚关闭的socket使用的IP和端口号
     int flag = 1;
     setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag));
 
-    /* 将监听描述符和监听socket的地址信息绑定上 */
+    // 将监听描述符和监听socket的地址信息绑定上
     ret = bind(listenfd, (struct sockaddr *)&address, sizeof(address));
     printf("bind return : %d", ret);
     assert(ret >= 0);
 
-    /* 监听指定端口号上任意IP地址的消息 */
+    // 监听指定端口号上任意IP地址的消息
     ret = listen(listenfd, 5);
     assert(ret >= 0);
 
-    epoll_event events[MAX_EVENT_NUMBER]; /* 存放监听到的内核事件的数组 */
-    epollfd = epoll_create(5);            /* 标识内核事件表的文件描述符真正建立了 */
+    epoll_event events[MAX_EVENT_NUMBER]; // 存放监听到的内核事件的数组
+    epollfd = epoll_create(5);            // 标识内核事件表的文件描述符真正建立了
     assert(epollfd != -1);
 
-    addfd(epollfd, listenfd, false); /* 把监听文件描述符listenfd加入监听表 */
-    http_conn::m_epollfd = epollfd;  /* http_conn类中m_epollfd其实就是主线程中的epollfd */
+    addfd(epollfd, listenfd, false); // 把监听文件描述符listenfd加入监听表
+    http_conn::m_epollfd = epollfd;  // http_conn类中m_epollfd其实就是主线程中的epollfd
 
-    /* 创建主线程和子线程之间通信用的管道
-    注意，用socketpair创建的管道pipefd[0]和pipefd[1]都是可读可写的，但一般还是用0读，1写 */
-    ret = socketpair(PF_UNIX, SOCK_STREAM, 0, pipefd);
+    // 创建主线程和子线程之间通信用的管道
+    注意，用socketpair创建的管道pipefd[0] 和pipefd[1] 都是可读可写的，但一般还是用0读，1写
+        ret = socketpair(PF_UNIX, SOCK_STREAM, 0, pipefd);
     assert(ret != -1);
 
-    /* 为什么管道写端要非阻塞？
-    send是将信息发送给套接字缓冲区，如果缓冲区满了，则会阻塞，这时候会进一步增加信号处理函数的执行时间，为此，将其修改为非阻塞。 */
-    setnonblocking(pipefd[1]);
+    // 为什么管道写端要非阻塞？
+    send是将信息发送给套接字缓冲区，如果缓冲区满了，则会阻塞，这时候会进一步增加信号处理函数的执行时间，为此，将其修改为非阻塞。
+        setnonblocking(pipefd[1]);
 
-    /* 监听管道的读端 */
+    // 监听管道的读端
     addfd(epollfd, pipefd[0], false);
 
-    /* 添加监听信号，不使用restart参数 */
+    // 添加监听信号，不使用restart参数
     addsig(SIGALRM, sig_handler, false);
     addsig(SIGTERM, sig_handler, false);
 
-    bool stop_server = false; /* 是否停止服务器运行 */
+    bool stop_server = false; // 是否停止服务器运行
 
-    client_data *users_timer = new client_data[MAX_FD]; /* 指向定时器链表中用户数据的指针变量，可用users_timer[fd]索引fd的用户数据*/
+    client_data *users_timer = new client_data[MAX_FD]; // 指向定时器链表中用户数据的指针变量，可用users_timer[fd]索引fd的用户数据
 
-    bool timeout = false; /* 是否超时？ */
-    alarm(TIMESLOT);      /* TIMESLOT秒后会触发SIGALRM信号 */
+    bool timeout = false; // 是否超时？
+    alarm(TIMESLOT);      // TIMESLOT秒后会触发SIGALRM信号
 
     while (!stop_server)
     {
-        int number = epoll_wait(epollfd, events, MAX_EVENT_NUMBER, -1); /* evnents上监听到的事件数量 */
+        int number = epoll_wait(epollfd, events, MAX_EVENT_NUMBER, -1); // evnents上监听到的事件数量
 
         if (number < 0 && errno != EINTR)
         {
@@ -199,24 +196,24 @@ int main(int argc, char *argv[])
             break;
         }
 
-        /* 依次遍历evnents上监听到的事件 */
+        // 依次遍历evnents上监听到的事件
         for (int i = 0; i < number; i++)
         {
-            int sockfd = events[i].data.fd; /* 存放所监听事件的文件描述符信息，方便判断如何处理接收到信息 */
+            int sockfd = events[i].data.fd; // 存放所监听事件的文件描述符信息，方便判断如何处理接收到信息
 
-            /* 有新的客户连接来了 */
+            // 有新的客户连接来了
             if (sockfd == listenfd)
             {
-                struct sockaddr_in client_address;                    /* 存放客户连接的socket地址信息 */
-                socklen_t client_addrlength = sizeof(client_address); /* 存放客户socket地址的长度 */
+                struct sockaddr_in client_address;                    // 存放客户连接的socket地址信息
+                socklen_t client_addrlength = sizeof(client_address); // 存放客户socket地址的长度
 
-/* LT模式 */
+// LT模式
 #ifdef listenfdLT
 
-                /* 存放所接受客户连接对应的文件描述符 */
+                // 存放所接受客户连接对应的文件描述符
                 int connfd = accept(listenfd, (struct sockaddr *)&client_address, &client_addrlength);
 
-                /* 判断是否有异常情况出现 */
+                // 判断是否有异常情况出现
                 if (connfd < 0)
                 {
                     LOG_ERROR("%s:errno is:%d", "accept error", errno);
@@ -229,37 +226,37 @@ int main(int argc, char *argv[])
                     continue;
                 }
 
-                /* 初始化connfd对应的http_conn对象里存放的数据 */
+                // 初始化connfd对应的http_conn对象里存放的数据
                 users[connfd].init(connfd, client_address);
 
                 // 初始化client_data数据
                 // 创建定时器，设置回调函数和超时时间，绑定用户数据，将定时器添加到链表中
 
-                /* 初始化定时器链表中结点对应的用户数据 */
+                // 初始化定时器链表中结点对应的用户数据
                 users_timer[connfd].address = client_address;
                 users_timer[connfd].sockfd = connfd;
 
-                /* 一个指向定时器链表中结点的指针变量，用于初始化 */
+                // 一个指向定时器链表中结点的指针变量，用于初始化
                 util_timer *timer = new util_timer;
-                timer->user_data = &users_timer[connfd]; /* 设置用户数据 */
-                timer->cb_func = cb_func;                /* 设置回调函数 */
+                timer->user_data = &users_timer[connfd]; // 设置用户数据
+                timer->cb_func = cb_func;                // 设置回调函数
 
-                /* 设置超时时间为3倍的TIMESLOT */
+                // 设置超时时间为3倍的TIMESLOT
                 time_t cur = time(NULL);
                 timer->expire = cur + 3 * TIMESLOT;
 
-                /* 有点套娃，用户数据里又存放了定时器链表中的结点信息 */
+                // 有点套娃，用户数据里又存放了定时器链表中的结点信息
                 users_timer[connfd].timer = timer;
 
-                /* 将新的定时器结点插入到定时器链表的正确位置 */
+                // 将新的定时器结点插入到定时器链表的正确位置
                 timer_lst.add_timer(timer);
 
 #endif
 
-/* ET模式
-可以看到ET模式需要一直接受新的连接，直到没有新的连接可以接受为止才会break，处理一下个内核事件 */
+                // ET模式
+                可以看到ET模式需要一直接受新的连接，直到没有新的连接可以接受为止才会break，处理一下个内核事件
 #ifdef listenfdET
-                while (1)
+                    while (1)
                 {
                     int connfd = accept(listenfd, (struct sockaddr *)&client_address, &client_addrlength);
                     if (connfd < 0)
@@ -291,10 +288,10 @@ int main(int argc, char *argv[])
 #endif
             }
 
-            /* 不管是哪个文件描述符（管道、客户连接）出现以下3个错误，我们都关闭这个连接并移除定时器
+            // 不管是哪个文件描述符（管道、客户连接）出现以下3个错误，我们都关闭这个连接并移除定时器
             但既然定时器都出来了，那难道不是只针对客户连接吗，管道好像没有对应的定时器才对？
             此外，定时器的作用不就是关闭非活动连接吗，所以更不可能扯上管道了
-            嗯，是这样的*/
+            嗯，是这样的
             else if (events[i].events & (EPOLLRDHUP | EPOLLHUP | EPOLLERR))
             {
                 // 服务器端关闭连接，移除对应的定时器
@@ -306,7 +303,7 @@ int main(int argc, char *argv[])
                 }
             }
 
-            /* 处理管道上的可读信号 */
+            // 处理管道上的可读信号
             else if ((sockfd == pipefd[0]) && (events[i].events & EPOLLIN))
             {
                 int sig;
@@ -326,14 +323,14 @@ int main(int argc, char *argv[])
                     {
                         switch (signals[i])
                         {
-                        /* 收到SIGALRM信号等会就要去处理非活动连接了 */
+                        // 收到SIGALRM信号等会就要去处理非活动连接了
                         case SIGALRM:
                         {
                             timeout = true;
                             break;
                         }
 
-                        /* 服务器停止运行的信号 */
+                        // 服务器停止运行的信号
                         case SIGTERM:
                         {
                             stop_server = true;
@@ -343,7 +340,7 @@ int main(int argc, char *argv[])
                 }
             }
 
-            /* 处理客户连接上接收到的数据 */
+            // 处理客户连接上接收到的数据
             else if (events[i].events & EPOLLIN)
             {
                 util_timer *timer = users_timer[sockfd].timer;
@@ -376,7 +373,7 @@ int main(int argc, char *argv[])
                 }
             }
 
-            /* 处理客户连接上的可写事件 */
+            // 处理客户连接上的可写事件
             else if (events[i].events & EPOLLOUT)
             {
                 util_timer *timer = users_timer[sockfd].timer;
