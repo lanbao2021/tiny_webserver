4653f94d68377c3d77d32a7ab751f0120c8d682b
supermicro <supermicro@scnu.com>
2023-10-20 22:56:10 +0800

    循环数组 + 生产者消费者模型实现的线程安全的阻塞队列

--- log/block_queue.h
+++ log/block_queue.h
@@ -1,8 +1,3 @@
-/*************************************************************
-*循环数组实现的阻塞队列，m_back = (m_back + 1) % m_max_size;  
-*线程安全，每个操作前都要先加互斥锁，操作完后，再解锁
-**************************************************************/
-
 #ifndef BLOCK_QUEUE_H
 #define BLOCK_QUEUE_H
 
@@ -11,239 +6,245 @@
 #include <pthread.h>
 #include <sys/time.h>
 #include "../lock/locker.h"
+
 using namespace std;
 
+/****************************************************************************************/
+/* STL容器都是非线程安全的，这是因为STL容器的设计者认为，线程安全的代价太高了，所以不提供线程安全的容器 */
+/* 循环数组实现的阻塞队列，m_back = (m_back + 1) % m_max_size;                              */
+/* 线程安全，每个操作前都要先加互斥锁，操作完后，再解锁。                                        */
+/****************************************************************************************/
+
 template <class T>
-class block_queue
+class block_queue // 循环数组 + 生产者消费者模型实现的线程安全的阻塞队列
 {
 
-    private:
-        locker m_mutex; /* 互斥锁 */
-        cond m_cond; /* 条件变量 */
+private:
+    locker m_mutex; // 互斥锁
+    cond m_cond;    // 条件变量
 
-        T *m_array; /* 阻塞队列，用循环数组来实现 */
-        int m_size; /* 记录阻塞队列中元素个数 */
-        int m_max_size; /* 阻塞队列中最多存放的元素个数 */
-        int m_front; /* 首元素的下标 */
-        int m_back; /* 尾元素的下标 */
-    
-    public:
+    T *m_array;     // 指向循环数组的指针
+    int m_size;     // 阻塞队列中当前元素的个数
+    int m_max_size; // 阻塞队列中最多存放的元素个数
+    int m_front;    // 队首元素的下标
+    int m_back;     // 队尾元素的下标
 
-        /* 构造函数，初始化私有成员 */
-        block_queue(int max_size = 1000)
+public:
+    // 构造函数，初始化私有成员
+    block_queue(int max_size = 1000)
+    {
+        if (max_size <= 0) // 阻塞队列中最多存放max_size个元素
         {
-            if (max_size <= 0)
-            {
-                exit(-1);
-            }
-
-            m_max_size = max_size;
-            m_array = new T[max_size];
-            m_size = 0;
-            m_front = -1;
-            m_back = -1;
+            exit(-1); // 退出程序
         }
 
-        /* 清除循环队列上的元素，不用真的删除，只要将相关索引重新初始化即可 */
-        void clear()
+        m_max_size = max_size;     // 阻塞队列中最多存放max_size个元素
+        m_array = new T[max_size]; // 创建循环数组
+        m_size = 0;                // 阻塞队列中当前元素个数
+        m_front = -1;              // 队首元素的下标
+        m_back = -1;               // 队尾元素的下标
+    }
+
+    // 清除循环队列上的元素（不用真的删除，只要将相关索引重新初始化即可）
+    void clear()
+    {
+        m_mutex.lock(); // 先获取互斥锁
+
+        m_size = 0;   // 阻塞队列中当前元素个数
+        m_front = -1; // 队首元素的下标
+        m_back = -1;  // 队尾元素的下标
+
+        m_mutex.unlock(); // 解锁
+    }
+
+    // 销毁循环阻塞队列
+    ~block_queue()
+    {
+        m_mutex.lock(); // 先获取互斥锁
+
+        if (m_array != NULL)  // 如果循环数组不为空
+            delete[] m_array; // 释放循环数组
+
+        m_mutex.unlock(); // 解锁
+    }
+
+    // 判断队列是否满了
+    bool full()
+    {
+        m_mutex.lock(); // 先获取互斥锁
+
+        if (m_size >= m_max_size) // 队列满了
         {
-            m_mutex.lock(); /* 先获取互斥锁 */
-            m_size = 0;
-            m_front = -1;
-            m_back = -1;
-            m_mutex.unlock(); /* 解锁 */
+            m_mutex.unlock(); // 解锁
+            return true;      // 返回true
         }
 
-        /* 销毁循环阻塞队列 */
-        ~block_queue()
-        {
-            m_mutex.lock(); /* 先获取互斥锁 */
-            
-            if (m_array != NULL)
-                delete [] m_array;
+        m_mutex.unlock(); // 解锁
 
-            m_mutex.unlock(); /* 解锁 */
+        return false;
+    }
+
+    // 判断队列是否为空
+    bool empty()
+    {
+        m_mutex.lock(); // 先获取互斥锁
+
+        if (0 == m_size) // 队列为空
+        {
+            m_mutex.unlock(); // 解锁
+            return true;      // 返回true
         }
 
+        m_mutex.unlock(); // 解锁
 
-        //判断队列是否满了
-        bool full() 
+        return false;
+    }
+
+    // 返回队首元素
+    bool front(T &value)
+    {
+        m_mutex.lock(); // 先获取互斥锁
+
+        if (0 == m_size)
         {
-            m_mutex.lock(); /* 先获取互斥锁 */
-            
-            if (m_size >= m_max_size)
-            {
-                m_mutex.unlock();
-                return true;
-            }
-            
             m_mutex.unlock();
-            
+            return false;
+        }
+        value = m_array[m_front]; // 返回队首元素
+        m_mutex.unlock();         // 解锁
+
+        return true;
+    }
+
+    // 返回队尾元素
+    bool back(T &value)
+    {
+        m_mutex.lock(); // 先获取互斥锁
+
+        if (0 == m_size) // 队列为空
+        {
+            m_mutex.unlock(); // 解锁
             return false;
         }
 
+        value = m_array[m_back]; // 返回队尾元素
 
-        //判断队列是否为空
-        bool empty() 
+        m_mutex.unlock(); // 解锁
+
+        return true;
+    }
+
+    int size()
+    {
+        int tmp = 0; // ！！！！因为不能直接返回m_size，否则互斥锁不好处理
+
+        m_mutex.lock(); // 加锁
+
+        tmp = m_size; // 获取返回队列中元素的个数
+
+        m_mutex.unlock(); // 解锁
+
+        return tmp;
+    }
+
+    int max_size()
+    {
+        int tmp = 0; // ！！！因为不能直接返回m_max_size，否则互斥锁不好处理
+
+        m_mutex.lock(); // 先获取互斥锁
+
+        tmp = m_max_size; // 获取返回队列中最多存放的元素个数
+
+        m_mutex.unlock(); // 解锁
+
+        return tmp;
+    }
+
+    // 往队列添加元素，需要将所有使用队列的线程先唤醒
+    // 当有元素push进队列,相当于生产者生产了一个元素
+    // 若当前没有线程等待条件变量,则唤醒无意义
+    bool push(const T &item)
+    {
+        m_mutex.lock(); // 先获取互斥锁
+
+        if (m_size >= m_max_size) // 队列满了
         {
-            m_mutex.lock(); /* 先获取互斥锁 */
 
-            if (0 == m_size)
+            m_cond.broadcast(); // 唤醒所有线程
+            m_mutex.unlock();   // 解锁
+            return false;       // 返回false
+        }
+
+        m_back = (m_back + 1) % m_max_size; // 队尾元素的下标
+        m_array[m_back] = item;             // 将item赋值给队尾元素
+
+        m_size++; // 队列中当前元素个数加1
+
+        m_cond.broadcast(); // 唤醒所有线程
+        m_mutex.unlock();   // 解锁
+        return true;
+    }
+
+    // pop时，如果当前队列没有元素，将会等待条件变量
+    bool pop(T &item)
+    {
+
+        m_mutex.lock(); // 先获取互斥锁
+
+        while (m_size <= 0) // 之所以用while，是因为pthread_cond_wait()可能会意外唤醒
+        {
+            if (!m_cond.wait(m_mutex.get())) // 这里多了一层封装要注意
+            {
+                // m_cond.wait封装了pthread_cond_wait()，而m_cond.wait的return语句是ret == 0
+                // 也就是说pthread_cond_wait()正确返回0时(等到了生产者生产），m_cond.wait返回1，而!1为false，所以会跳出循环，正常pop
+                m_mutex.unlock(); 
+                return false;     
+            }
+        }
+
+        m_front = (m_front + 1) % m_max_size; // 队首元素的下标
+        item = m_array[m_front];              // 将队首元素赋值给item
+        m_size--;                             // 队列中当前元素个数减1
+
+        m_mutex.unlock(); // 解锁
+
+        return true;
+    }
+
+    // 增加了超时处理
+    bool pop(T &item, int ms_timeout)
+    {
+        struct timespec t = {0, 0};
+        struct timeval now = {0, 0};
+        gettimeofday(&now, NULL);
+
+        m_mutex.lock(); // 先获取互斥锁
+
+        if (m_size <= 0)
+        {
+            t.tv_sec = now.tv_sec + ms_timeout / 1000;
+            t.tv_nsec = (ms_timeout % 1000) * 1000;
+            if (!m_cond.timewait(m_mutex.get(), t))
             {
                 m_mutex.unlock();
-                return true;
+                return false;
             }
+        }
 
+        if (m_size <= 0) // 二重if的原因是pthread_cond_timedwait()可能会意外唤醒
+        {
             m_mutex.unlock();
-
             return false;
         }
 
+        m_front = (m_front + 1) % m_max_size;
+        item = m_array[m_front];
+        m_size--;
 
-        //返回队首元素
-        bool front(T &value) 
-        {
-            m_mutex.lock(); /* 先获取互斥锁 */
+        m_mutex.unlock();
 
-            if (0 == m_size)
-            {
-                m_mutex.unlock();
-                return false;
-            }
-            value = m_array[m_front];
-            m_mutex.unlock();
-
-            return true;
-        }
-
-
-        //返回队尾元素
-        bool back(T &value) 
-        {
-            m_mutex.lock(); /* 先获取互斥锁 */
-
-            if (0 == m_size)
-            {
-                m_mutex.unlock();
-                return false;
-            }
-            value = m_array[m_back];
-
-            m_mutex.unlock();
-
-            return true;
-        }
-
-        int size() 
-        {
-            int tmp = 0; // 因为不能直接返回m_size，否则互斥锁不好处理
-
-            m_mutex.lock(); /* 先获取互斥锁 */
-
-            tmp = m_size;
-
-            m_mutex.unlock();
-            return tmp;
-        }
-
-        int max_size()
-        {
-            int tmp = 0; // 因为不能直接返回m_max_size，否则互斥锁不好处理
-
-            m_mutex.lock(); /* 先获取互斥锁 */
-
-            tmp = m_max_size;
-
-            m_mutex.unlock();
-            return tmp;
-        }
-
-        //往队列添加元素，需要将所有使用队列的线程先唤醒
-        //当有元素push进队列,相当于生产者生产了一个元素
-        //若当前没有线程等待条件变量,则唤醒无意义
-        bool push(const T &item)
-        {
-
-            m_mutex.lock(); /* 先获取互斥锁 */
-            
-            if (m_size >= m_max_size)
-            {
-
-                m_cond.broadcast();
-                m_mutex.unlock();
-                return false;
-            }
-
-            m_back = (m_back + 1) % m_max_size;
-            m_array[m_back] = item;
-
-            m_size++;
-
-            m_cond.broadcast();
-            m_mutex.unlock();
-            return true;
-        }
-
-
-        //pop时,如果当前队列没有元素,将会等待条件变量
-        bool pop(T &item)
-        {
-
-            m_mutex.lock(); /* 先获取互斥锁 */
-
-            while (m_size <= 0)
-            {
-                
-                if (!m_cond.wait(m_mutex.get()))
-                {
-                    m_mutex.unlock();
-                    return false;
-                }
-            }
-
-            m_front = (m_front + 1) % m_max_size;
-            item = m_array[m_front];
-            m_size--;
-
-            m_mutex.unlock();
-
-            return true;
-        }
-
-        //增加了超时处理
-        bool pop(T &item, int ms_timeout)
-        {
-            struct timespec t = {0, 0};
-            struct timeval now = {0, 0};
-            gettimeofday(&now, NULL);
-            
-            m_mutex.lock(); /* 先获取互斥锁 */
-
-            if (m_size <= 0)
-            {
-                t.tv_sec = now.tv_sec + ms_timeout / 1000;
-                t.tv_nsec = (ms_timeout % 1000) * 1000;
-                if (!m_cond.timewait(m_mutex.get(), t))
-                {
-                    m_mutex.unlock();
-                    return false;
-                }
-            }
-
-            if (m_size <= 0)
-            {
-                m_mutex.unlock();
-                return false;
-            }
-
-            m_front = (m_front + 1) % m_max_size;
-            item = m_array[m_front];
-            m_size--;
-
-            m_mutex.unlock();
-            
-            return true;
-        }
+        return true;
+    }
 };
 
 #endif
--- log/log.h
+++ log/log.h
@@ -10,58 +10,69 @@
 
 using namespace std;
 
+// 日志类
 class Log
 {
-public:
-    //C++11以后,使用局部变量懒汉不用加锁
-    static Log *get_instance()
+
+private:
+    char dir_name[128];               // 路径名
+    char log_name[128];               // log文件名
+    int m_split_lines;                // 日志最大行数
+    int m_log_buf_size;               // 日志缓冲区大小
+    long long m_count;                // 日志行数记录
+    int m_today;                      // 因为按天分类,记录当前时间是那一天
+    FILE *m_fp;                       // 打开log的文件指针
+    char *m_buf;                      // 写缓冲区
+    block_queue<string> *m_log_queue; // 阻塞队列
+    bool m_is_async;                  // 是否同步标志位
+    locker m_mutex;                   // 互斥锁，用来保护日志缓冲区
+
+private:
+    Log();                  // 单例模式，构造函数私有化，防止外部new
+    virtual ~Log();         // 析构函数私有化，防止外部delete
+    void *async_write_log() // 异步写日志方法
     {
-        static Log instance;
-        return &instance;
+        string single_log; // 单条日志
+
+        while (m_log_queue->pop(single_log)) // 从阻塞队列中取出一条日志
+        {
+            m_mutex.lock(); // 加锁
+
+            fputs(single_log.c_str(), m_fp); // 写入文件
+
+            m_mutex.unlock(); // 解锁
+        }
     }
 
+public:
+    // 对外接口
+
+    // 获取单例对象（C++11以后，使用局部变量懒汉不用加锁）
+    static Log *get_instance()
+    {
+        static Log instance; // C++11以后，局部静态变量线程安全
+        return &instance;    // 返回单例对象的地址
+    }
+
+    // 异步写日志公有方法，调用私有方法async_write_log
     static void *flush_log_thread(void *args)
     {
         Log::get_instance()->async_write_log();
     }
-    
-    //可选择的参数有日志文件、日志缓冲区大小、最大行数以及最长日志条队列
+
+    // 可选择的参数有日志文件、日志缓冲区大小、最大行数以及最长日志条队列
     bool init(const char *file_name, int log_buf_size = 8192, int split_lines = 5000000, int max_queue_size = 0);
 
+    // 将输出内容按照标准格式整理
+    // 写入日志内容的函数，可变参数，最后一个参数为可变参数的个数
     void write_log(int level, const char *format, ...);
 
+    // 强制刷新缓冲区
     void flush(void);
-
-private:
-    Log();
-    virtual ~Log();
-    void *async_write_log()
-    {
-        string single_log;
-        //从阻塞队列中取出一个日志string，写入文件
-        while (m_log_queue->pop(single_log))
-        {
-            m_mutex.lock();
-            fputs(single_log.c_str(), m_fp);
-            m_mutex.unlock();
-        }
-    }
-
-private:
-    char dir_name[128]; //路径名
-    char log_name[128]; //log文件名
-    int m_split_lines;  //日志最大行数
-    int m_log_buf_size; //日志缓冲区大小
-    long long m_count;  //日志行数记录
-    int m_today;        //因为按天分类,记录当前时间是那一天
-    FILE *m_fp;         //打开log的文件指针
-    char *m_buf;
-    block_queue<string> *m_log_queue; //阻塞队列
-    bool m_is_async;                  //是否同步标志位
-    locker m_mutex;
 };
 
-//日志类中的方法都不会被其他程序直接调用，下面四个可变参数宏提供了其他程序的调用方法，用于不同类型的日志输出
+// 日志类中的方法都不会被其他程序直接调用（所以定义成public有啥用*.*)
+// 下面四个可变参数宏提供了其他程序的调用方法，用于不同类型的日志输出
 #define LOG_DEBUG(format, ...) Log::get_instance()->write_log(0, format, ##__VA_ARGS__)
 #define LOG_INFO(format, ...) Log::get_instance()->write_log(1, format, ##__VA_ARGS__)
 #define LOG_WARN(format, ...) Log::get_instance()->write_log(2, format, ##__VA_ARGS__)
